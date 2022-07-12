d59da890e2083b9e7b7bc2e0152a9925179cd01d
HataYuki <yhata.01@gmail.com>
2022-07-12 21:02:41 +0900

    add api

--- pages/work/[slug]/index.tsx
+++ pages/work/[slug]/index.tsx
@@ -2,16 +2,23 @@ import type {NextPage, GetServerSideProps} from 'next'
 import Root from "../../../parts/object/project/root";
 import PageHeadLine from "../../../parts/object/component/pageHeadLine";
 import Project from "../../../parts/object/project/project";
-import type {Post} from "@lib/posts";
-import {getAPostBySlug} from "@lib/posts";
+import type {Post, PostIdSlugList} from "@lib/posts";
+import {getPost, getPostIdSlugList} from "@lib/posts";
 
 interface Props {
     post: Post
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const projectName = (context.params && context.params.aProject) ? context.params.aProject : '';
-    const post = await getAPostBySlug(projectName)
+    const slug = (context.params && context.params.slug) ? context.params.slug : '';
+    const idSlugList = await getPostIdSlugList()
+    const aIdSlug = idSlugList.filter(idSlug => idSlug.slug === slug)
+    let id: string = ''
+    if (aIdSlug.length) {
+        id = aIdSlug[0].id
+    }
+
+    const post = await getPost(id)
     return {
         props: {
             post
