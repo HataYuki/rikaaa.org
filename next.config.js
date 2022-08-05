a03ab51a07e4494b475a43a49b4852e457fcbb42
HataYuki <yhata.01@gmail.com>
2022-08-05 20:15:03 +0900

    Fix purge-css setting

--- next.config.js
+++ next.config.js
@@ -9,7 +9,33 @@ const nextConfig = {
   swcMinify: true,
   images:{
     domains:['storage.googleapis.com']
-  }
+  },
+  plugins: [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        "autoprefixer": {
+          "flexbox": "no-2009"
+        },
+        "stage": 3,
+        "features": {
+          "custom-properties": false
+        }
+      }
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}'
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ["html", "body"]
+      }
+    ],
+  ]
 }
 
-module.exports = withPurgeCss(withBundleAnalyzer(nextConfig))
+module.exports = withBundleAnalyzer(nextConfig)
