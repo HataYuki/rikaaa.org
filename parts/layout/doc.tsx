69a0da64d233a0286e82a66800cb46874c2b7fde
HataYuki <yhata.01@gmail.com>
2022-08-05 23:07:45 +0900

    Cancel to self-host font

--- parts/layout/doc.tsx
+++ parts/layout/doc.tsx
@@ -31,13 +31,10 @@ const Doc = ({children, postIndexList}: Props) => {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover"/>
-                <link
-                    rel={'preload'}
-                    href={'/fonts/NotoSansJP-Thin.otf'}
-                    as={'font'}
-                    type={'font/otf'}
-                    crossOrigin={''}
-                />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={""}/>
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap"
+                      rel="stylesheet"/>
                 <title>rikaaa.org</title>
             </Head>
             <Script
